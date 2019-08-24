15154bf8bc2a75ecfaed68dad68e153029012fbb
yuewang0321 <yuewang0321@gmail.com>
2019-08-24 15:40:31 -0400

    FakeNewsComponent Added

---
+++ frontend/src/components/FakeNews.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import '../styles/FakeNews.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationArrow } from "@fortawesome/free-solid-svg-icons";
+import { border } from '@material-ui/system';
+
+const newsList = [
+    {
+    'title': 'Google GoogleGoogle GoogleGoogle GoogleGoogle GoogleGoogle GoogleGoogle GoogleGoogle GoogleGoogle GoogleGoogle Google',
+    'link': 'https://www.google.com/',
+    'publishedAt': '2019-01-01'
+    },
+    {
+    'title': 'Facebook',
+    'link': 'https://www.facebook.com/',
+    'publishedAt': '2019-01-01'
+    },
+    {
+    'title': 'Acorn',
+    'link': 'https://twitter.com/',
+    'publishedAt': '2019-01-01'
+    },
+    {
+    'title': 'Google Google',
+    'link': 'https://www.google.com/',
+    'publishedAt': '2019-01-01'
+    },
+    {
+    'title': 'Facebook',
+    'link': 'https://www.facebook.com/',
+    'publishedAt': '2019-01-01'
+    },
+    {
+    'title': 'Acorn',
+    'link': 'https://twitter.com/',
+    'publishedAt': '2019-01-01'
+    }
+]
+
+
+
+export default function FakeNews() {
+    const news = newsList;
+    // const classes = useStyles();
+    return (
+        <ul>
+            {news.map((item) => <li key={item.title}>
+            <Card className="card">
+            <CardContent>
+            <Typography className='title'>{item.title}</Typography>
+            </CardContent>
+            <CardActions  className='learnmore'>
+
+                <div className='publishdate'>{item.publishedAt}</div>
+                <a href={item.link} target="_blank">
+                    <button className='btn'>
+                        <FontAwesomeIcon className='icon' icon = { faLocationArrow } />
+                    </button>
+                </a>
+            </CardActions>
+            </Card>
+                
+                </li>)}
+        </ul>
+    )
+}
