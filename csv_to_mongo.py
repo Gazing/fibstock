ef53450874cd6420016919538b5a29a519a556ef
danny <hyeong.yu@mail.utoronto.ca>
2019-08-23 22:22:59 -0400

    data added and dataclean added

--- csv_to_mongo.py
+++ csv_to_mongo.py
@@ -6,16 +6,29 @@ import pymongo
 import sys, getopt, pprint
 
 #CSV to JSON Conversion
-csvfile = open('./data/trainReal.csv', 'r')
+csvfile = open('./data/trainFake.csv', 'r')
 reader = csv.DictReader( csvfile )
 myclient = pymongo.MongoClient("18.219.233.150:27017")
 database = myclient['fibstock']
-collection = database['trainingData']
+collection = database['train']
 header= ["title", "link", "publishedAt", "isFake"]
+
+req = []
 
 for each in reader:
     row={}
     for field in header:
+        if field == "title":
+            # Remove punctation
+            each[field] = each[field].replace('[^\w\s]', ' ')
+            # Remove numbers
+            each[field] = each[field].replace('[^A-Za-z]', ' ')
+            # Make sure any double-spaces are single
+            each[field] = each[field].replace('  ', ' ')
+            each[field] =  each[field].replace('  ', ' ')
+            # Transform all text to lowercase
+            each[field] = each[field].lower()
         row[field]=each[field]
-    collection.insert_one(row)
+    req.append(row)
+collection.insert_many(req)
 myclient.close()
