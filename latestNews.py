42410b3353c1ad15f7853d5b90be59c464a627b1
ZiXian(Wannie) Chen <Gazing@users.noreply.github.com>
2019-08-24 16:43:23 -0400

    Added: new ml model for determining if news is relevant (#3)

--- latestNews.py
+++ latestNews.py
@@ -4,24 +4,33 @@ import json
 import pandas as pd
 import pymongo
 
-input = input('Enter company name: ')
-url = 'https://newsapi.org/v2/everything?q=' + input + '&pageSize=100&apiKey=efd324e53e494216b672fcea329bb317'
+# input = input('Enter company name: ')
 
-response = requests.get(url)
-jsonRes = response.json()
+
+companies = ["Nvidia", "Adobe", "Twitter", "Comcast", "AT&T", "Verizon", "News Corp.", "Fox", "Walmart", "Target", "Costco"]
 
 myclient = pymongo.MongoClient("18.219.233.150:27017")
 database = myclient['fibstock']
 collection = database['news']
-
 coll = []
-for res in jsonRes.get('articles'):
-    row = {
-        'title': res.get('title'),
-        'link': res.get('url'),
-        'publishedAt': res.get("publishedAt")
-    }
-    coll.append(row)
+check = {}
+
+for company in companies:
+    url = 'https://newsapi.org/v2/everything?q=' + company + '&pageSize=100&apiKey=efd324e53e494216b672fcea329bb317'
+
+    response = requests.get(url)
+    jsonRes = response.json()
+
+
+    for res in jsonRes.get('articles'):
+        row = {
+            'title': res.get('title'),
+            'link': res.get('url'),
+            'publishedAt': res.get("publishedAt")
+        }
+        if (row["title"] not in check):
+            coll.append(row)
+            check[row["title"]] = 1
 
 collection.insert_many(coll)
 
