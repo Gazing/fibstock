7a20a83fb61cdc86e5dd5e7fafd5b5614b0879e4
danny <hyeong.yu@mail.utoronto.ca>
2019-08-24 20:10:44 -0400

    push

--- frontend/src/api/fibstockAPI.js
+++ frontend/src/api/fibstockAPI.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export function api (query) {
     return fetch("http://127.0.0.1:8000/api/companies?q=" + query);
 }
@@ -7,10 +6,3 @@ export function getNewsForCompany(company) {
     let url = "http://127.0.0.1:8000/api/companies/" + company + "/news";
     return fetch(url);
 }
-=======
-export function api (company) {
-    return fetch("http://127.0.0.1:8000/api/companies/" + company + "/news");
-}
-
->>>>>>> 24d2f8114f9e11940fd209d6a3c0464da30a52fe
-
